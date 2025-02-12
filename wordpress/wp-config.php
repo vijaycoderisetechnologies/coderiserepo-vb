87419c95e9dff4b9d9acabde94f48393c70d8fac
vijaycoderisetechnologies <vijay.baghel@coderisetechnologies.com>
2025-02-12 10:32:38 +0000

    helm chart update

--- wordpress/wp-config.php
+++ wordpress/wp-config.php
@@ -1,105 +1,47 @@
+
 <?php
-/**
- * The base configuration for WordPress
- *
- * The wp-config.php creation script uses this file during the installation.
- * You don't have to use the web site, you can copy this file to "wp-config.php"
- * and fill in the values.
- *
- * This file contains the following configurations:
- *
- * * Database settings
- * * Secret keys
- * * Database table prefix
- * * ABSPATH
- *
- * @link https://wordpress.org/support/article/editing-wp-config-php/
- *
- * @package WordPress
- */
+
+
 define('WP_MEMORY_LIMIT', '1024M');
 define('FORCE_SSL_ADMIN', true);
 
-/* Turn HTTPS 'on' if HTTP_X_FORWARDED_PROTO matches 'https' */
-if (strpos($_SERVER['HTTP_X_FORWARDED_PROTO'], 'https') !== false) {
-    $_SERVER['HTTPS'] = 'on';
-}
-$_SERVER['HTTPS'] = 'on';
-define( 'WP_HOME', 'https://coderise.io' );
-define( 'WP_SITEURL', 'https://coderise.io' );
+/* Get Minikube IP dynamically */
+$minikube_ip = trim(shell_exec("minikube ip"));
 
-// ** Database settings - You can get this info from your web host ** //
-/** The name of the database for WordPress */
-define('DB_NAME', 'crwordpress');
+/* Get NodePort dynamically */
+$wp_port = trim(shell_exec("kubectl get svc wordpress -o=jsonpath='{.spec.ports[0].nodePort}'"));
 
-/** MySQL database username */
-define('DB_USER', 'cradmindbuser');
+/* Set WordPress home and site URL */
+define( 'WP_HOME', 'http://' . $minikube_ip . ':' . $wp_port );
+define( 'WP_SITEURL', 'http://' . $minikube_ip . ':' . $wp_port );
 
-/** MySQL database password */
-define('DB_PASSWORD', 'BEvHEZD4hCP$$');
+/* Database */
+define('DB_NAME', getenv('WORDPRESS_DB_NAME') ?: 'wordpress_db');
+define('DB_USER', getenv('WORDPRESS_DB_USER') ?: 'wordpress');
+define('DB_PASSWORD', getenv('WORDPRESS_DB_PASSWORD') ?: 'wordpress-password');
+define('DB_HOST', getenv('WORDPRESS_DB_HOST') ?: 'mysql');
 
-/** MySQL hostname */
-define('DB_HOST', 'crwp-database.cjsie1hnt1hi.us-east-1.rds.amazonaws.com:3306');
+/* Security Keys */
+define('AUTH_KEY', 'unique-key');
+define('SECURE_AUTH_KEY', 'unique-key');
+define('LOGGED_IN_KEY', 'unique-key');
+define('NONCE_KEY', 'unique-key');
+define('AUTH_SALT', 'unique-key');
+define('SECURE_AUTH_SALT', 'unique-key');
+define('LOGGED_IN_SALT', 'unique-key');
+define('NONCE_SALT', 'unique-key');
 
-/** Database Charset to use in creating database tables. */
-define('DB_CHARSET', 'utf8');
-
-/** The Database Collate type. Don't change this if in doubt. */
-define('DB_COLLATE', '');
-
-/**#@+
- * Authentication unique keys and salts.
- *
- * Change these to different unique phrases! You can generate these using
- * the {@link https://api.wordpress.org/secret-key/1.1/salt/ WordPress.org secret-key service}.
- *
- * You can change these at any point in time to invalidate all existing cookies.
- * This will force all users to have to log in again.
- *
- * @since 2.6.0
- */
-define( 'AUTH_KEY',         'put your unique phrase here' );
-define( 'SECURE_AUTH_KEY',  'put your unique phrase here' );
-define( 'LOGGED_IN_KEY',    'put your unique phrase here' );
-define( 'NONCE_KEY',        'put your unique phrase here' );
-define( 'AUTH_SALT',        'put your unique phrase here' );
-define( 'SECURE_AUTH_SALT', 'put your unique phrase here' );
-define( 'LOGGED_IN_SALT',   'put your unique phrase here' );
-define( 'NONCE_SALT',       'put your unique phrase here' );
-
-/**#@-*/
-
-/**
- * WordPress database table prefix.
- *
- * You can have multiple installations in one database if you give each
- * a unique prefix. Only numbers, letters, and underscores please!
- */
+/* Table prefix */
 $table_prefix = 'wp_';
 
-/**
- * For developers: WordPress debugging mode.
- *
- * Change this to true to enable the display of notices during development.
- * It is strongly recommended that plugin and theme developers use WP_DEBUG
- * in their development environments.
- *
- * For information on other constants that can be used for debugging,
- * visit the documentation.
- *
- * @link https://wordpress.org/support/article/debugging-in-wordpress/
- */
-define( 'WP_DEBUG', false );
+/* Debugging */
+define('WP_DEBUG', false);
 
-/* Add any custom values between this line and the "stop editing" line. */
-
-
-/* That's all, stop editing! Happy publishing. */
-
-/** Absolute path to the WordPress directory. */
-if ( ! defined( 'ABSPATH' ) ) {
-	define( 'ABSPATH', __DIR__ . '/' );
+/* Absolute path to the WordPress directory */
+if (!defined('ABSPATH')) {
+    define('ABSPATH', dirname(__FILE__) . '/');
 }
 
-/** Sets up WordPress vars and included files. */
+/* Load WordPress settings */
 require_once ABSPATH . 'wp-settings.php';
+
